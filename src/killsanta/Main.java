0a0aef5cfff625c29fdba974418d592a393f5e22
Hugo Winbladh <hugowinbladh@gmail.com>
2016-12-22 18:02:42 +0100

    kill santa

--- src/killsanta/Main.java
+++ src/killsanta/Main.java
@@ -1,12 +1,18 @@
 package killsanta;
 
+import java.io.*;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
 import javax.swing.*;
+import sun.audio.*;
 
 public class Main extends JFrame{
 
     Panel panel;
     
+    
     public Main(){
+        InputStream is = getClass().getClassLoader().getResourceAsStream("sounds/samp.wav");
         panel = new Panel();
         addKeyListener(panel);
         setBounds(0,0,800,600);
@@ -18,7 +24,25 @@ public class Main extends JFrame{
     }
     
     public static void main(String[] args) {
+        play("res/music.wav");
         new Main();
     }
+    
+    public static void play(String filename)
+{
+    try
+    {
+        Clip clip = AudioSystem.getClip();
+        clip.open(AudioSystem.getAudioInputStream(new File(filename)));
+        clip.start();
+    }
+    catch (Exception exc)
+    {
+        exc.printStackTrace(System.out);
+    }
+}
+    
+    
+    
     
 }
